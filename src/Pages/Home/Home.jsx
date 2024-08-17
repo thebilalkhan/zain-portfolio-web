9b3fe486b24ccad28464cc8833a071c173ea0b54
thebilalkhan <bilalkhanusafzai786@gmail.com>
2024-08-16 23:11:27 -0700

    chanes in home section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
           <div className=' text-xl lg:text-2xl xl:text-3xl 2xl:text-4xl'>Hi 👋</div>
           <div className=' text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl' >I'm <span className='text-red-600'>Bilal,</span> </div>
           <div className=' text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl'>Frontend Web Developer</div>
-          <div className=' text-sm lg:text-lg xl:text-xl 2xl:text-2xl'>I have 3+ years of experience as a Frontend Web Developer (HTML, CSS, Javascript,Bootstrap, Tailwind CSS ) Included 1 years as a Reacts Js Developer. Experienced with React Redux and Context Api.</div>
+          <div className=' text-sm lg:text-lg xl:text-xl 2xl:text-2xl'>I have 2+ years of experience as a Frontend Web Developer (HTML, CSS, Javascript,Bootstrap, Tailwind CSS ) Included 1 years as a Reacts Js Developer. Experienced with React Redux and Context Api.</div>
           <div>  <button className='bg-gray-900  rounded-md text-white p-2 my-4 px-4 text-sm lg:text-xl'>Get in touch </button></div>
         </div>
 
